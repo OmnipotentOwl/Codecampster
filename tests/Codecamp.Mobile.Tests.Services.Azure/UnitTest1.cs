46c1245f003543f7685a9041533301090c02d592
OmnipotentOwl <1769881+OmnipotentOwl@users.noreply.github.com>
2019-11-03 11:23:57 -0500

    Setup tests placeholder

---
+++ tests/Codecamp.Mobile.Tests.Services.Azure/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Codecamp.Mobile.Tests.Services.Azure
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
